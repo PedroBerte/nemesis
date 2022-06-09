9aca2becc522a4ce8349a9ac4cad1bc4e5359e57
PedroBerte <pedrobertelli19@gmail.com>
2022-06-09 19:40:44 -0300

    "toast.promise" adicionada na tela de Registro

---
+++ src/Pages/GymSpecs/GymSpecs.jsx
@@ -0,0 +1,10 @@
+const GymSpecs = () => {
+  return (
+    <div className="GymSpecs-body">
+      <div className="GymSpecs-left-side"></div>
+      <div className="GymSpecs-right-side"></div>
+    </div>
+  );
+};
+
+export default GymSpecs;
