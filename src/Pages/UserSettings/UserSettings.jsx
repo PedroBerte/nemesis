25f52bdb33a22e42531c189b60b0558b5e462d55
PedroBerte <pedrobertelli19@gmail.com>
2022-06-27 12:13:39 -0300

    Adicinada a pasta .github para deploy no gitPages

--- src/Pages/UserSettings/UserSettings.jsx
+++ src/Pages/UserSettings/UserSettings.jsx
@@ -339,7 +339,12 @@ const UserSettings = () => {
               Se você apagar a sua conta, nunca mais terá acesso à ela!
             </h4>
           </div>
-          <img className="modal-small-logo" width="35px" src={smallLogo} alt="" />
+          <img
+            className="modal-small-logo"
+            width="35px"
+            src={smallLogo}
+            alt=""
+          />
         </div>
         <Input
           type="email"
